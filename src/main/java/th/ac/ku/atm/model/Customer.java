e6b2cd38f483087ab59e4714f8a2c7a5739c1b4f
kidstylex <chilizhiki@gmail.com>
2020-10-09 22:11:30 +0700

    JPA work

--- src/main/java/th/ac/ku/atm/model/Customer.java
+++ src/main/java/th/ac/ku/atm/model/Customer.java
@@ -1,9 +1,17 @@
 package th.ac.ku.atm.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id
     private int id;
     private String name;
     private String pin;
+
+    public Customer() {}
 
     public Customer(int id, String name, String pin) {
         this.id = id;
@@ -35,12 +43,4 @@ public class Customer {
         this.pin = pin;
     }
 
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", pin=" + pin +
-                '}';
-    }
 }
